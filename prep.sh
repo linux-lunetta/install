1b8cae6ec0aa22831c338071a6035d9a19aac713
Al Muhdil Karim <almuhdilkarim2@gmail.com>
2025-11-22 22:21:16 +0700

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -17,89 +17,23 @@ if [[ ! -e /dev/mapper/data  ]]; then
 	cryptsetup luksOpen $DISKDATA data
 fi
 
-if [[ ! -e /dev/mapper/proc ]]; then
-	exit
-fi
-
-if [[ ! -e /dev/mapper/data ]]; then
-	exit
-fi
-
-if [[ -z $( vgs | grep proc ) ]]; then
-	pvcreate /dev/mapper/proc &&
-	vgcreate proc /dev/mapper/proc
-fi
-
-if [[ -z $( vgs | grep data ) ]]; then
-	pvcreate /dev/mapper/data &&
-	vgcreate data /dev/mapper/data
-fi
-
-
-PROCDISK=(root opts droi vars nets conf vlog vtmp vpac vaud temp docs)
-DATADISK=(home)
-
-
-for n in "${PROCDISK[@]}"
-do
-	if [[ ! -z /dev/proc/$n ]]; then
-  		mkfs.ext4 -F -q -b 4096 /dev/proc/$n
-	fi
-done
-
-for n in "${DATADISK[@]}"
-do
-	if [[ ! -z /dev/data/$n ]]; then
-   		mkfs.ext4 -F -q -b 4096 /dev/data/$n
-	fi
-done
-
 
 mkfs.vfat -F32 -S 4096 -n BOOT $DISKBOOT
+mkfs.ext4 -F -q -b 4096 /dev/proc &&
+mkfs.ext4 -F -q -b 4096 /dev/data
 
-if [[ -e /dev/data/host ]];then
-	mkfs.btrfs -f /dev/data/host
-fi
-
-
-if [[ -e /dev/data/dock ]];then
-  mkfs.btrfs -f /dev/data/dock
-fi
-
-
-mount /dev/proc/root /mnt &&
-
-mkdir -p /mnt/{boot,home,opt,var,tmp,srv/http} && 
-
+mount /dev/proc /mnt &&
+mkdir -p /mnt/boot &&
 mount -o uid=0,gid=0,dmask=007,fmask=007 $DISKBOOT /mnt/boot/ &&
-mount /dev/proc/opts /mnt/opt &&
-mount /dev/proc/vars /mnt/var &&
-mount /dev/proc/temp /mnt/tmp &&
-mount /dev/proc/docs /mnt/srv/http &&
-mount /dev/data/home /mnt/home &&
+mkdir -p /mnt/home &&
+mount /dev/data /mnt/home &&
 
-
-mkdir -p /mnt/var/{tmp,log,cache/pacman,lib/telnet,lib/config,lib/waydroid} &&
-mount /dev/proc/vtmp /mnt/var/tmp &&
-mount /dev/proc/nets /mnt/var/lib/telnet &&
-mount /dev/proc/conf /mnt/var/lib/config &&
-mount /dev/data/dock /mnt/var/lib/waydroid &&
-
-
-mount /dev/proc/vpac /mnt/var/cache/pacman &&
-
-
-mkdir -p /mnt/var/log/audit &&
-mount /dev/proc/vaud /mnt/var/log/audit &&
-
-
-mkdir -p /mnt/home/media &&
-mount /dev/data/repo /mnt/home/media &&
 
 pacstrap /mnt base &&
 
 genfstab -U /mnt > /mnt/etc/fstab &&
 cp -fr $(pwd)/post /mnt &&
+
 
 echo "[multilib]" >> /mnt/etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
